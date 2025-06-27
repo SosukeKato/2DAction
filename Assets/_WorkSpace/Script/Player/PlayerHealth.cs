cb10fc6e30a18d6e5918765492a135ea1ef231ec
SosukeKato <dokindokinpiano@gmail.com>
2025-06-27 15:13:33 +0900

    [Update]PlayerHealth.csに変数Healthを宣言し、その数値(そのままHPになる)を100に設定

--- Assets/_WorkSpace/Script/Player/PlayerHealth.cs
+++ Assets/_WorkSpace/Script/Player/PlayerHealth.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField]
+    public int Health = 100;
     // Start is called before the first frame update
     void Start()
     {
