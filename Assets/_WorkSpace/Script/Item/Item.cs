96a87bd2204a611b5281ae41bdbec2a796b4f811
SosukeKato <dokindokinpiano@gmail.com>
2025-06-29 22:04:59 +0900

    [add]Item.csを追加

--- Assets/_WorkSpace/Script/Item/Item.cs
+++ Assets/_WorkSpace/Script/Item/Item.cs
@@ -2,22 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSkill : MonoBehaviour
+public class Item : MonoBehaviour
 {
-    Transform _tr;
     // Start is called before the first frame update
     void Start()
     {
-        _tr = transform;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //シールド追従スキル
-        if (Input.GetKeyDown(KeyCode.O))
-        {
-            
-        }
+        
     }
 }
