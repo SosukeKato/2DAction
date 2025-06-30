03b6c6832bfa5eed5b474ec6683b8e976f105600
SosukeKato <dokindokinpiano@gmail.com>
2025-06-30 16:59:56 +0900

    [Update]FlyerPositionClamp.csにx軸の移動範囲制限を追加

--- Assets/_WorkSpace/Script/Enemy/Flyer/FlyerPositionClamp.cs
+++ Assets/_WorkSpace/Script/Enemy/Flyer/FlyerPositionClamp.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class FlyerPositionClamp : MonoBehaviour
 {
+    public Transform _tr;
     [SerializeField]
     float _minY = 2f;
     [SerializeField]
     float _maxY = 5f;
+    [SerializeField]
+    float _minX = -11f;
+    [SerializeField]
+    float _maxX = 11f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _tr = transform;
     }
 
     // Update is called once per frame
@@ -19,8 +24,9 @@ public class FlyerPositionClamp : MonoBehaviour
     {
         Vector2 pos = transform.position;
 
+        pos.x = Mathf.Clamp(pos.x, _minX, _maxX);
         pos.y = Mathf.Clamp(pos.y, _minY, _maxY);
 
-        transform.position = pos;
+        _tr.position = pos;
     }
 }
