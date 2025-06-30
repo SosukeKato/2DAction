ed1b774dd7f31d117c6e6a9edb4cf99087e57c6b
SosukeKato <dokindokinpiano@gmail.com>
2025-06-30 16:33:42 +0900

    [Update]EnemySpawner.csにListでSpawnをランダムで選択し、Enemyをランダムに生成する処理を追加

--- Assets/_WorkSpace/Script/Enemy/EnemySpawner.cs
+++ Assets/_WorkSpace/Script/Enemy/EnemySpawner.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField]
+    List<GameObject> _EnemyPrefab;
+    [SerializeField]
+    List<Transform> _EnemySpawn;
+    [SerializeField]
+    float _SpawnInterval;
+    [SerializeField]
+    float _SpawnTimer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,14 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _SpawnTimer += Time.deltaTime;
+
+        if (_SpawnTimer >= _SpawnInterval)
+        {
+            int Random = UnityEngine.Random.Range(0, _EnemySpawn.Count);
+            int RandomEnemy = UnityEngine.Random.Range(0, _EnemyPrefab.Count);
+            Instantiate(_EnemyPrefab[RandomEnemy], _EnemySpawn[Random].position, Quaternion.identity);
+            _SpawnTimer = 0f;
+        }
     }
 }
