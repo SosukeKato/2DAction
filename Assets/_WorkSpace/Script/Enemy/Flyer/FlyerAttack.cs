4bdffd87c7cef4e1dc040b8bf5f244f1d560ed18
SosukeKato <dokindokinpiano@gmail.com>
2025-08-09 21:43:43 +0900

    [Update]FlyerAttackをアタッチするとFlyerに必要なコンポーネントを自動でアタッチする処理を追加

--- Assets/_WorkSpace/Script/Enemy/Flyer/FlyerAttack.cs
+++ Assets/_WorkSpace/Script/Enemy/Flyer/FlyerAttack.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(EnemyAI))]
+[RequireComponent (typeof(EnemyHealth))]
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent (typeof(Collider2D))]
 public class FlyerAttack : MonoBehaviour
 {
+    [SerializeField]
+    GameObject _flyerAttack;
+    [SerializeField]
+    Transform _flyer;
     // Start is called before the first frame update
     void Start()
     {
