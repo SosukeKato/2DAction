344b7e561540fe3e7913ae96480167eaf313b803
SosukeKato <dokindokinpiano@gmail.com>
2025-08-15 13:26:17 +0900

    [Update]PlayerもしくはEnemyに衝突したとき攻撃の判定を消す処理を追加

--- Assets/_WorkSpace/Script/Destroy/AttackObject.cs
+++ Assets/_WorkSpace/Script/Destroy/AttackObject.cs
@@ -6,9 +6,17 @@ public class AttackObject : MonoBehaviour
 {
     [SerializeField]
     float _AttackDuration;
-    private IEnumerator Start()
+    IEnumerator Start()
     {
         yield return new WaitForSeconds(_AttackDuration);
         Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
