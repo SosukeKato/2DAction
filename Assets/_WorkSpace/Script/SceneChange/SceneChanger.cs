0ccc0796fe278bb09fb273fd93bdd86b533e7cdb
SosukeKato <dokindokinpiano@gmail.com>
2025-08-21 18:31:19 +0900

    [Update]Sceneを切り替える処理を追加

--- Assets/_WorkSpace/Script/SceneChange/SceneChanger.cs
+++ Assets/_WorkSpace/Script/SceneChange/SceneChanger.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChanegeScene(string StageName)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(StageName);
     }
 }
