f329465cf7a95c948c3ba8fa63450fcfef45c35c
SosukeKato <dokindokinpiano@gmail.com>
2025-08-22 21:12:16 +0900

    [Update]SceneChanger.csにSceneManagerをsingletonにする処理を追加

--- Assets/_WorkSpace/Script/SceneChange/SceneChanger.cs
+++ Assets/_WorkSpace/Script/SceneChange/SceneChanger.cs
@@ -3,8 +3,5 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public void ChanegeScene(string StageName)
-    {
-        SceneManager.LoadScene(StageName);
-    }
+    static SceneChanger instance { get; set; }
 }
