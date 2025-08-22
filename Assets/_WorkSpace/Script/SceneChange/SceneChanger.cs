11eba6dec99ac217b9eabf01ce4dcffd6cd4a4a4
SosukeKato <dokindokinpiano@gmail.com>
2025-08-22 21:13:58 +0900

    [fix]Singletonにするのに足りなかった処理を追加

--- Assets/_WorkSpace/Script/SceneChange/SceneChanger.cs
+++ Assets/_WorkSpace/Script/SceneChange/SceneChanger.cs
@@ -4,4 +4,17 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     static SceneChanger instance { get; set; }
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
