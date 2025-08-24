79dc43833fdc8021230deaba3bad0751081f2fe9
SosukeKato <dokindokinpiano@gmail.com>
2025-08-24 21:16:33 +0900

    TitleSceneにいる時にTitlwBGMを再生するための処理を追加

--- Assets/_WorkSpace/Script/Manager/AudioController.cs
+++ Assets/_WorkSpace/Script/Manager/AudioController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioController : MonoBehaviour
 {
@@ -23,6 +24,9 @@ public class AudioController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (SceneManager.GetActiveScene().name == "TitleScene")
+        {
+
+        }
     }
 }
