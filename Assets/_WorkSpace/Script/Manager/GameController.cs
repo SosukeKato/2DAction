a0706d340998ab9c11ec89ba31bb18fbbb069672
SosukeKato <dokindokinpiano@gmail.com>
2025-08-26 22:06:50 +0900

    [Update]ゲーム内の時間を計測する処理をGameController.csに追加

--- Assets/_WorkSpace/Script/Manager/GameController.cs
+++ Assets/_WorkSpace/Script/Manager/GameController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    float _sec;
+    public float _min;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,13 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        #region Timer‚Ìˆ—
+        _sec += Time.deltaTime;
+        if (_sec >= 60 )
+        {
+            _sec = 0;
+            _min++;
+        }
+        #endregion
     }
 }
