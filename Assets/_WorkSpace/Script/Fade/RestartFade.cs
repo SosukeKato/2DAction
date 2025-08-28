cf2661dff8e92996c11daf45c4cdd2bfb8bd056c
SosukeKato <dokindokinpiano@gmail.com>
2025-08-29 02:27:58 +0900

    [Update]一度暗転してからシーンを切り替える処理を追加

--- Assets/_WorkSpace/Script/Fade/RestartFade.cs
+++ Assets/_WorkSpace/Script/Fade/RestartFade.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RestartFade : MonoBehaviour
 {
+    bool Fade = false;
+    byte _alpha;
+    [SerializeField]
+    Image _fade;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Fade)
+        {
+            FadeOut();
+        }
+        else
+        {
+            FadeIn();
+        }
+    }
+
+    void FadeOut()
+    {
+        _alpha++;
+        _fade.color = new Color32(0, 0, 0, _alpha);
+        if (_alpha == 255)
+        {
+            Fade = true;
+            SceneManager.LoadScene("ChangeScene");
+        }
+    }
+
+    void FadeIn()
+    {
+        _alpha--;
+        _fade.color = new Color32(0, 0, 0, _alpha);
+        if (_alpha == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
