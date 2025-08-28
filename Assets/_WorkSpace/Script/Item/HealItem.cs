a41d14c055bafa5d027ff3d9fc31433cac4b6e05
SosukeKato <dokindokinpiano@gmail.com>
2025-08-28 23:23:31 +0900

    [Update]Itemを取得する処理を追加

--- Assets/_WorkSpace/Script/Item/HealItem.cs
+++ Assets/_WorkSpace/Script/Item/HealItem.cs
@@ -8,16 +8,17 @@ public class HealItem : Item
 
     [SerializeField]
     int _itemHeal;
-    // Start is called before the first frame update
     void Start()
     {
         _pH = GetComponent<PlayerHealth>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            CatchItem();
+        }
     }
 
     public override void CatchItem()
