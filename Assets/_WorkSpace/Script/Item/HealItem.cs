a52bd14b0c44a66cad966b3c37ab38771129d21e
SosukeKato <dokindokinpiano@gmail.com>
2025-08-29 00:48:30 +0900

    [fix]HealItem.csがItem.csを継承するようにしていたのをやめてHealItem.csのみで処理をする形に変更

--- Assets/_WorkSpace/Script/Item/HealItem.cs
+++ Assets/_WorkSpace/Script/Item/HealItem.cs
@@ -2,27 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealItem : Item
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent (typeof(Rigidbody2D))]
+public class HealItem : MonoBehaviour
 {
     PlayerHealth _pH;
 
     [SerializeField]
-    int _itemHeal;
+    float _minY = -2.25f;
+    [SerializeField]
+    float _maxY = 5f;
+    [SerializeField]
+    int _heal = 3;
+
     void Start()
     {
         _pH = GetComponent<PlayerHealth>();
+    }
+
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        pos.y = Mathf.Clamp(pos.y, _minY, _maxY);
+        transform.position = pos;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            CatchItem();
+            _pH._playerHealth += _heal;
+            Destroy(gameObject);
         }
-    }
-
-    public override void CatchItem()
-    {
-        _pH._playerHealth += _itemHeal;
     }
 }
