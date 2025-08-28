87e2d2b6e40bb2ebed7c329e763b6b111906d9b3
SosukeKato <dokindokinpiano@gmail.com>
2025-08-28 23:20:10 +0900

    [Update]HealItem.cs内にPlayerHealthを回復する処理を追加

--- Assets/_WorkSpace/Script/Item/HealItem.cs
+++ Assets/_WorkSpace/Script/Item/HealItem.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealItem : MonoBehaviour
+public class HealItem : Item
 {
+    PlayerHealth _pH;
+
+    [SerializeField]
+    int _itemHeal;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _pH = GetComponent<PlayerHealth>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public override void CatchItem()
+    {
+        _pH._playerHealth += _itemHeal;
     }
 }
