238f025ebb28e2f9df4b6881557804deea4ff1ef
SosukeKato <dokindokinpiano@gmail.com>
2025-09-05 05:40:38 +0900

    [Update]FlyerBulletがPlayerに向かって飛んでいくように変更

--- Assets/_WorkSpace/Script/AllEntity/Enemy/AllEnemy/FlyerAttack.cs
+++ Assets/_WorkSpace/Script/AllEntity/Enemy/AllEnemy/FlyerAttack.cs
@@ -1,12 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof(EnemyAI))]
-[RequireComponent (typeof(EnemyHealth))]
-[RequireComponent (typeof(Rigidbody2D))]
-[RequireComponent (typeof(Collider2D))]
-[RequireComponent (typeof(PositionClamp))]
+[RequireComponent(typeof(EnemyAI))]
+[RequireComponent(typeof(EnemyHealth))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(PositionClamp))]
 public class FlyerAttack : MonoBehaviour
 {
     [SerializeField]
@@ -17,18 +18,47 @@ public class FlyerAttack : MonoBehaviour
     GameObject _flyerAttackObject;
     float _flyerAttackInterval;
 
+
+
+    Transform _player;
+    [SerializeField]
+    float _bulletSpeed;
+
     void Start()
     {
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj == null)
+        {
+            throw new NullReferenceException("playerObj is null");
+        }
+        _player = playerObj.transform;
     }
 
     void Update()
     {
+        #region 自力
         _flyerAttackInterval += Time.deltaTime;
         if (_flyerAttackInterval >= 10)
         {
             _flyerAttackInterval = 0;
             _flyerAttackObject = Instantiate(_flyerAttack, _flyer.position, transform.rotation);
         }
+        #endregion
+
+        #region AI
+        // プレイヤーへの方向を計算
+        Vector2 direction = (_player.position - _flyer.position).normalized;
+
+        // 弾にRigidbody2Dがある場合、速度を設定
+        Rigidbody2D bulletRb = _flyerAttackObject.GetComponent<Rigidbody2D>();
+        if (bulletRb != null)
+        {
+            bulletRb.velocity = direction * _bulletSpeed;
+        }
+
+        // 弾の向きを設定（オプション）
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        _flyerAttackObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        #endregion
     }
 }
