8d2d2ee2a158bf0326a968a07c69b00a31b250a2
SosukeKato <dokindokinpiano@gmail.com>
2025-09-07 18:31:46 +0900

    [Update]BSC.csにシーンを切り替える処理を追加

--- Assets/_WorkSpace/Script/Manager/ButtonSceneChange.cs
+++ Assets/_WorkSpace/Script/Manager/ButtonSceneChange.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonSceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    string SceneName;
+    void OnClincked()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(SceneName);
     }
 }
