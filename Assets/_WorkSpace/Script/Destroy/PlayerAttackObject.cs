ba4dfc4cc295f4d26882fbbc0c86e64a48b62115
SosukeKato <dokindokinpiano@gmail.com>
2025-09-10 18:06:06 +0900

    SkillAttackObject.csの名前をPlayerAttackObject.csに変更

--- Assets/_WorkSpace/Script/Destroy/PlayerAttackObject.cs
+++ Assets/_WorkSpace/Script/Destroy/PlayerAttackObject.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class SkillAttackObject : MonoBehaviour
+public class PlayerAttackObject : MonoBehaviour
 {
     [SerializeField]
     float _attackDuration;
