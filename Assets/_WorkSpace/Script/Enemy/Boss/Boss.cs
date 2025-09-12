c3276b52e49aec42704974f4de6aa5072c03b664
SosukeKato <dokindokinpiano@gmail.com>
2025-09-12 18:43:35 +0900

    [Update]指定の秒数を超えるとBossが攻撃する処理を追加(攻撃自体の処理は未実装)

--- Assets/_WorkSpace/Script/Enemy/Boss/Boss.cs
+++ Assets/_WorkSpace/Script/Enemy/Boss/Boss.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    int _bossAttackInterval = 10;
+
+    float _bossAttackTimer;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _bossAttackTimer += Time.deltaTime;
+        if (_bossAttackTimer >= _bossAttackInterval)
+        {
+            
+        }
     }
 }
