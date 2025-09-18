34e80f8cf3724761c4b5e4c3a9b55e655b3658e3
SosukeKato <dokindokinpiano@gmail.com>
2025-09-18 22:41:01 +0900

    [Update]Bossの攻撃の処理を追加

--- Assets/_WorkSpace/Script/Enemy/Boss/Boss.cs
+++ Assets/_WorkSpace/Script/Enemy/Boss/Boss.cs
@@ -1,9 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
     [SerializeField]
     int _bossAttackInterval = 10;
+    [SerializeField]
+    List<GameObject> _bossAttackObj;
+    [SerializeField]
+    List<Transform> _bossAttackPos;
 
     float _bossAttackTimer;
     void Start()
@@ -16,7 +21,7 @@ public class Boss : MonoBehaviour
         _bossAttackTimer += Time.deltaTime;
         if (_bossAttackTimer >= _bossAttackInterval)
         {
-            
+            Instantiate(_bossAttackObj[1], _bossAttackPos[1].position, Quaternion.identity);
         }
     }
 }
