5644757fc008a3945e0f76fdaf71d2f1fef3ef0a
SosukeKato <dokindokinpiano@gmail.com>
2025-09-24 18:37:33 +0900

    [Update]PlayerHealthの変数を取得できるようにする処理を追加

--- Assets/_WorkSpace/Script/Destroy/PlayerAttackObject.cs
+++ Assets/_WorkSpace/Script/Destroy/PlayerAttackObject.cs
@@ -5,10 +5,28 @@ public class PlayerAttackObject : MonoBehaviour
 {
     [SerializeField]
     float _attackDuration;
+
+    GameObject _playerObj;
+    PlayerHealth _pH;
+
+    private void Awake()
+    {
+        _playerObj = GameObject.FindGameObjectWithTag("Player");
+        _pH = _playerObj.GetComponent<PlayerHealth>();
+    }
+
     IEnumerator Start()
     {
         Application.targetFrameRate = 30;
         yield return new WaitForSeconds(_attackDuration);
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+
+        }
     }
 }
