01cefd5e40c961de5311f3a63175831a79a4ff41
SosukeKato <dokindokinpiano@gmail.com>
2025-09-25 19:25:39 +0900

    PlayerAttackObject.cs内の不要な処理を削除

--- Assets/_WorkSpace/Script/Destroy/PlayerAttackObject.cs
+++ Assets/_WorkSpace/Script/Destroy/PlayerAttackObject.cs
@@ -8,27 +8,10 @@ public class PlayerAttackObject : MonoBehaviour
     [SerializeField]
     int _bulletHeal = 15;
 
-    GameObject _playerObj;
-    PlayerHealth _pH;
-
-    private void Awake()
-    {
-        _playerObj = GameObject.FindGameObjectWithTag("Player");
-        _pH = _playerObj.GetComponent<PlayerHealth>();
-    }
-
     IEnumerator Start()
     {
         Application.targetFrameRate = 30;
         yield return new WaitForSeconds(_attackDuration);
         Destroy(gameObject);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            _pH._playerHealth += _bulletHeal;
-        }
     }
 }
