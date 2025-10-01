0e9fc932ab624a8dbb9d96b81b144b70cd47fa17
SosukeKato <dokindokinpiano@gmail.com>
2025-10-01 13:08:29 +0900

    [Update]Bossの左右の見た目を制御する処理を追加

--- Assets/_WorkSpace/Script/Enemy/Boss/Boss.cs
+++ Assets/_WorkSpace/Script/Enemy/Boss/Boss.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
+    Transform _tr;
+
     [SerializeField]
     int _bossAttackInterval = 10;
     [SerializeField]
@@ -12,17 +14,23 @@ public class Boss : MonoBehaviour
     Transform _player;
     GameObject _playerObj;
 
+    float _playerX;
+    float _bossX;
+
     float _bossAttackTimer;
     int _randomBossAttack;
     int _randomBossAttackPos;
     void Start()
     {
+        _tr = transform;
+
         _playerObj = GameObject.FindGameObjectWithTag("Player");
         _player = _playerObj.transform;
     }
 
     void Update()
     {
+        #region BossÇÃçUåÇèàóù
         _bossAttackTimer += Time.deltaTime;
         if (_bossAttackTimer >= _bossAttackInterval)
         {
@@ -31,5 +39,20 @@ public class Boss : MonoBehaviour
             Instantiate(_bossAttackObj[_randomBossAttack], _bossAttackPos[_randomBossAttackPos].position, Quaternion.identity);
             _bossAttackTimer = 0;
         }
+        #endregion
+
+        #region BossÇÃç∂âEÇÃå©ÇΩñ⁄ÇÃí≤êÆ
+        _playerX = _player.position.x;
+        _bossX = _tr.position.x;
+
+        if (_playerX - _bossX < 0)
+        {
+            _tr.eulerAngles = new Vector3(0, 0, 0);
+        }
+        if (_playerX - _bossX > 0)
+        {
+            _tr.eulerAngles = new Vector3(0, 180, 0);
+        }
+        #endregion
     }
 }
